ca0c7a64e679873a63e1e3506999e433e7640650
Maksym Padalka <sheihmaks@gmail.com>
2023-10-18 22:47:26 +0200

    +

--- src/components/ui/Feedback/Feedback.jsx
+++ src/components/ui/Feedback/Feedback.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from 'components/share/Button';
 import styles from './Feedback.module.scss';
 
+
 const Feedback = ({ toggleModal }) => {
   return (
     <Button
