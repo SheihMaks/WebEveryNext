c5fd9f2b0ca2e916dd5c5cc46b88dde14d71146f
Maksym Padalka <sheihmaks@gmail.com>
2023-10-23 11:28:37 +0200

    Update NavBar.jsx

--- src/components/layout/NavBar/NavBar.jsx
+++ src/components/layout/NavBar/NavBar.jsx
@@ -28,7 +28,7 @@ const path=usePathname()
     }
   }
   return (<header className={styles.headerNav}>
-      <Link href='/' className={styles.navLinkLogo} prefetch={false}>
+      <Link href='/' className={styles.navLinkLogo} passHref prefetch={false}>
         <Logo
           className={styles.laptopLogo}
           width="145"
